d4ad9e46490e56a8fc6c8464806f9d45ef11c3b4
Maria Andrea Rodriguez <mandrea.rodriguezf@gmail.com>
2022-05-15 20:52:41 -0400

    new folders

--- _tests_/engineer.test.js
+++
@@ -1,7 +0,0 @@
-const Engineer = require('../lib/engineer');
-
-test('get engineer github via method', () => {
-    const github = 'fmandrea30';
-    const newEngineer = new Engineer(1, 'maria', 'maria@hotmail.com', github);
-    expect(newEngineer.getGithub()).toBe(github);
-});
--- _tests_/manager.test.js
+++
@@ -1,7 +0,0 @@
-const Manager = require('../lib/manager');
-
-test('get manager office number via method', () => {
-    const officeNumber = 7854;
-    const newManager = new Manager(1, 'maria', 'maria@hotmail.com', officeNumber);
-    expect(newManager.getOfficeNumber()).toBe(officeNumber);
-});
